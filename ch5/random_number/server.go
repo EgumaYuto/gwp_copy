ff14843a74e413dc4db4a6be7ffd731e5c316abb
yuto.eguma <yuto.eguma@gmail.com>
2021-09-14 23:07:40 +0900

    if分岐を含んだテンプレートの例

---
+++ ch5/random_number/server.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"math/rand"
+	"net/http"
+	"text/template"
+	"time"
+)
+
+func process(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("tmpl.html")
+	rand.Seed(time.Now().Unix())
+	t.Execute(w, rand.Intn(10) > 5)
+}
+
+func main() {
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+	}
+	http.HandleFunc("/process", process)
+	server.ListenAndServe()
+}
