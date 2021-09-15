baa9ca5c3d1f76ce658a2b13b20f705c4d4d0c3d
yuto.eguma <yuto.eguma@gmail.com>
2021-09-16 07:43:25 +0900

    gocheck のテストスイートを構造体にした場合の設定

---
+++ ch8/sample_gocheck_2/server_test.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	. "gopkg.in/check.v1"
+)
+
+type PostTestSuite struct {
+	mux    *http.ServeMux
+	post   *FakePost
+	writer *httptest.ResponseRecorder
+}
+
+func init() {
+	Suite(&PostTestSuite{})
+}
+
+func Test(t *testing.T) { TestingT(t) }
+
+func (s *PostTestSuite) SetUpTest(c *C) {
+	s.post = &FakePost{}
+	s.mux = http.NewServeMux()
+	s.mux.HandleFunc("/post/", handleRequest(s.post))
+	s.writer = httptest.NewRecorder()
+}
+
+func (s *PostTestSuite) TestGetPost(c *C) {
+	request, _ := http.NewRequest("GET", "/post/1", nil)
+	s.mux.ServeHTTP(s.writer, request)
+
+	c.Check(s.writer.Code, Equals, 200)
+
+	var post Post
+	json.Unmarshal(s.writer.Body.Bytes(), &post)
+	c.Check(post.Id, Equals, 1)
+}
+
+func (s *PostTestSuite) TestPutPost(c *C) {
+	json := strings.NewReader(`{"content":"Updated post","author":"Sau Sheong"}`)
+	request, _ := http.NewRequest("PUT", "/post/1", json)
+	s.mux.ServeHTTP(s.writer, request)
+
+	c.Check(s.writer.Code, Equals, 200)
+	c.Check(s.post.Id, Equals, 1)
+	c.Check(s.post.Content, Equals, "Updated post")
+}
