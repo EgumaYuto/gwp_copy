97bccaff9d23291e2f0c05378c2b6a659034b1f4
yuto.eguma <yuto.eguma@gmail.com>
2021-09-16 07:23:09 +0900

    DBをDIしたテスト

---
+++ ch8/httptest_2/fake_post.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	_ "github.com/lib/pq"
+)
+
+type FakePost struct {
+	Id      int
+	Content string
+	Author  string
+}
+
+func (post *FakePost) fetch(id int) (err error) {
+	post.Id = id
+	return
+}
+
+func (post *FakePost) create() (err error) {
+	return
+}
+
+func (post *FakePost) update() (err error) {
+	return
+}
+
+func (post *FakePost) delete() (err error) {
+	return
+}
