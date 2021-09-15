86f5bc575e2e456c68557bab1c22bb46359b0f4e
yuto.eguma <yuto.eguma@gmail.com>
2021-09-15 12:03:41 +0900

    xssが抑制される様子

---
+++ ch5/xss/server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func process(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("tmpl.html")
+	t.Execute(w, r.FormValue("comment"))
+}
+
+func form(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("form.html")
+	t.Execute(w, nil)
+}
+
+func main() {
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+	}
+	http.HandleFunc("/process", process)
+	http.HandleFunc("/form", form)
+	server.ListenAndServe()
+}
