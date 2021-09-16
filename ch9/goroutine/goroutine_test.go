208484ad23f43a48f74d3783d257263118747c58
yuto.eguma <yuto.eguma@gmail.com>
2021-09-16 09:57:25 +0900

    ゴルーチンのベンチマークテスト

--- ch9/goroutine/goroutine_test.go
+++ ch9/goroutine/goroutine_test.go
@@ -2,19 +2,42 @@ package main
 
 import (
 	"testing"
-	"time"
 )
 
-func TestPrint1(t *testing.T) {
-	print1()
+// func TestPrint1(t *testing.T) {
+// 	print1()
+// }
+
+// func TestGPrint1(t *testing.T) {
+// 	goPrint1()
+// 	time.Sleep(1 * time.Millisecond)
+// }
+
+// func TestGPrint2(t *testing.T) {
+// 	goPrint2()
+// 	time.Sleep(1 * time.Millisecond)
+// }
+
+func BenchmarkPrint1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		print1()
+	}
 }
 
-func TestGPrint1(t *testing.T) {
-	goPrint1()
-	time.Sleep(1 * time.Millisecond)
+func BenchmarkGoPrint1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		goPrint1()
+	}
 }
 
-func TestGPrint2(t *testing.T) {
-	goPrint2()
-	time.Sleep(1 * time.Millisecond)
+func BenchmarkPrint2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		print2()
+	}
+}
+
+func BenchmarkGoPrint2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		goPrint2()
+	}
 }
